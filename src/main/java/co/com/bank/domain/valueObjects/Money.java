06bd6b209711a0dba3057d7e8360749a8631e020
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-18 18:18:58 -0500

    Created the Money object value

---
+++ src/main/java/co/com/bank/domain/valueObjects/Money.java
@@ -0,0 +1,31 @@
+package co.com.bank.domain.valueObjects;
+
+public class Money {
+    private final Double value;
+
+    private Money(Double value) {
+        this.value = value;
+    }
+
+    public static Money valueOf(Double value) {
+        return new Money(value);
+    }
+
+    public static Money valueOf(String value) {
+        return new Money(Double.valueOf(value));
+    }
+
+    public Money sum(Money value) {
+        Double result = value.getValue() + this.value;
+        return new Money(result);
+    }
+
+    public Money subtract(Money value) {
+        Double result = this.value - value.getValue();
+        return new Money(result);
+    }
+
+    public Double getValue() {
+        return value;
+    }
+}
