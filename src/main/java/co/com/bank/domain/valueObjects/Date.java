270c42fda37ed545fed2021cf47d53a8c8795c19
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-18 18:44:52 -0500

    Created the Date value object

---
+++ src/main/java/co/com/bank/domain/valueObjects/Date.java
@@ -0,0 +1,36 @@
+package co.com.bank.domain.valueObjects;
+
+import java.time.LocalDateTime;
+
+public class Date {
+    private final LocalDateTime value;
+
+    public Date() {
+        this.value = LocalDateTime.now();
+    }
+
+    public Date(LocalDateTime value) {
+        this.value = value;
+    }
+
+    private static Boolean hasValidFormat(String string) {
+        return string.matches("\\d{4}-\\d{2}-\\d{2}");
+    }
+
+    /**
+     * This method parses a String into a valid Date object.
+     * @param string A string that follows this pattern: YYYY-MM-DD.
+     * @return Date
+     */
+    public static Date of(String string) {
+        if(!hasValidFormat(string)) {
+            throw new IllegalArgumentException("The string cannot be parsed since it doesn't follow the right format.");
+        }
+        LocalDateTime parsableString = LocalDateTime.parse(string);
+        return new Date(parsableString);
+    }
+
+    public LocalDateTime getValue() {
+        return value;
+    }
+}
