2a7747253cb27ebe0a76b717385e759d70bc1ed4
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-18 17:09:53 -0500

    Created an implementation of IIdGenerator

---
+++ src/main/java/co/com/bank/domain/utils/UUIDGenerator.java
@@ -0,0 +1,21 @@
+package co.com.bank.domain.utils;
+
+import co.com.bank.interfaces.IIdGenerator;
+
+import java.util.UUID;
+
+public class UUIDGenerator implements IIdGenerator {
+    private UUID getUUIDFromString(String string) {
+        return UUID.fromString(string);
+    }
+
+    @Override
+    public String generate() {
+        return UUID.randomUUID().toString();
+    }
+
+    @Override
+    public Boolean equals(String a, String b) {
+        return getUUIDFromString(a).equals(getUUIDFromString(b));
+    }
+}
--- src/main/java/co/com/bank/interfaces/IIdGenerator.java
+++ src/main/java/co/com/bank/interfaces/IIdGenerator.java
@@ -2,4 +2,5 @@ package co.com.bank.interfaces;
 
 public interface IIdGenerator {
     String generate();
+    Boolean equals(String string, String id);
 }
