f051c427aea0e7c936afd2d3c0dca48231e2ae04
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-18 19:11:15 -0500

    Created the Holder entity

---
+++ src/main/java/co/com/bank/entities/Holder.java
@@ -0,0 +1,28 @@
+package co.com.bank.entities;
+
+import co.com.bank.domain.valueObjects.Password;
+
+public class Holder extends Person {
+    private Password password;
+
+    public Holder(String name, String password) {
+        super(name);
+        this.password = new Password(password);
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPassword() {
+        return this.password.getValue();
+    }
+
+    public void setPassword(String password) {
+        this.password = new Password(password);
+    }
+}
--- src/main/java/co/com/bank/entities/Person.java
+++ src/main/java/co/com/bank/entities/Person.java
@@ -1,6 +1,16 @@
 package co.com.bank.entities;
 
+import co.com.bank.domain.utils.UUIDGenerator;
+import co.com.bank.interfaces.IIdGenerator;
+
 public abstract class Person {
+    private static final IIdGenerator idGenerator = new UUIDGenerator();
+    // -----------
     protected String id;
     protected String name;
+
+    public Person(String name) {
+        this.id = idGenerator.generate();
+        this.name = name;
+    }
 }
